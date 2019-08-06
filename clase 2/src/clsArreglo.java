7a66c65e1d757dae278d66f8aafc331497516b28
jose-federi <53129468+jose-federi@users.noreply.github.com>
2019-08-05 22:24:32 -0600

    Add files via upload

---
+++ clase 2/src/clsArreglo.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Usuarios
+ */
+public class clsArreglo 
+{
+  public void ejemplo1(){
+      int [] numero=new int[5];
+      
+      hy6
+  }  
+}
+
